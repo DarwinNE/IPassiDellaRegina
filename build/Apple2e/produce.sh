a0e533ecc5e10b6addc0aade287ff748309a50c6
Fabio Battaglia <tabaglio@posteo.net>
2026-01-16 21:08:36 +0100

    Add code to handle splash screen

--- build/Apple2e/produce.sh
+++ build/Apple2e/produce.sh
@@ -7,12 +7,12 @@
 
 # Assemble the disk image
 cp dsk/prodos.po ./queens_it.po
-java -jar $acjarfile -as ./queens_it.po QUEENS.SYSTEM < queens_it.bin
+java -jar $acjarfile -as ./queens_it.po STARTUP < splash.bin
+java -jar $acjarfile -as ./queens_it.po GAME < queens_it.bin
+java -jar $acjarfile -p ./queens_it.po SPLASH.HGR 0 < dsk/splash.hgr
 java -jar $acjarfile -p ./queens_it.po EM.DRV 0 < dsk/a2e.auxmem.emd
 java -jar $acjarfile -p ./queens_it.po TEXT.DAT 0 < text_it.dat
 
-cp ../readme.txt .
-
 rm  AppleIIe_Queens_IT.zip
-zip -r AppleIIe_Queens_IT.zip queens_it.po readme.txt note_Apple2e.txt
+zip -r AppleIIe_Queens_IT.zip queens_it.po
 cp AppleIIe_Queens_IT.zip $ditdir
